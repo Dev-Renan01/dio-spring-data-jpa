2a0d350078a63ae334e18a83f93d66de04d0e5f8
Thiago Renan <thiagorenan1108@gmail.com>
2025-06-04 13:37:01 -0300

    Inserindo dados no MySQL

--- spring-data-jpa/src/main/java/dio/spring_data_jpa/StartApp.java
+++ spring-data-jpa/src/main/java/dio/spring_data_jpa/StartApp.java
@@ -14,12 +14,29 @@ public class StartApp implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        User user = new User();
-        user.setName("Thiago Renan");
-        user.setUserName("RenanDev");
-        user.setPassword("123456");
+        User user1 = new User();
+        user1.setName("Suany Souza");
+        user1.setUserName("SuanyDev");
+        user1.setPassword("1436");
+        repository.save(user1);
 
-        repository.save(user);
+        User user2 = new User();
+        user2.setName("Renan");
+        user2.setUserName("RDev");
+        user2.setPassword("1241456");
+        repository.save(user2);
+
+        User user3 = new User();
+        user3.setName("Regis");
+        user3.setUserName("RegisDev");
+        user3.setPassword("123455436");
+        repository.save(user3);
+
+        User user4 = new User();
+        user4.setName("Irani");
+        user4.setUserName("Iranilda");
+        user4.setPassword("12345654");
+        repository.save(user4);
 
         for(User u : repository.findAll()){
             System.out.println(u);
